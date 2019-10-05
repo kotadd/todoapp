5c0cda62396fe87a8110f320a363ed38bc79d6ec
Kota Nishinaka <knishina@yahoo-corp.jp>
2019-10-05 16:17:57 +0900

    Create AddTodo and VisibleTodoList components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
+
+const App = () => (
+  <>
+    <AddTodo />
+    <VisibleTodoList />
+    {/* <Footer /> */}
+  </>
+);
+
+export default App;
